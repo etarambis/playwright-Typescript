6705b09e71b63a17d1281c1db2e31dd5a3b9fdf3
Edison <etarambis@gmail.com>
2026-04-12 22:25:57 -0500

    Refactor Login BP

--- tests/banco_pichincha/loginRedBanco.spec.ts
+++ tests/banco_pichincha/loginRedBanco.spec.ts
@@ -15,36 +15,30 @@ test('loginRedBanco', async ({ }) => {
 
     await page.waitForTimeout(2000);
 
-    await page.getByRole('textbox', { name: 'usuario@dominio.com' }).fill('usrpqaau@pichincha.com');
+    await page.getByRole('textbox', { name: 'usuario@dominio.com' }).fill('etarambi@pichincha.com');
     await page.locator('#idSIButton9').click();
     await page.locator('#i0118').click();
-    await page.locator('#i0118').fill('GHtyrf4*8_3DaqJs');
+    await page.locator('#i0118').fill('Judith12t4576..');
     await page.locator("#idSIButton9").click();
-    await expect(page.getByRole('table', { name: 'table-component' })).toBeVisible();
 
+    
+    await page.waitForTimeout(5000);
     await page.getByRole('button', { name: 'search Búsqueda avanzada' }).click();
 
-    await expect(page.getByRole('button', { name: 'close Cerrar' })).toBeVisible();
-
     await page.getByText('Selecciona 1 empresa').click();
-    await expect(page.getByRole('option', { name: 'Todos' })).toBeVisible();
-
     await page.getByRole('option', { name: 'Lucagiros' }).click();
     await page.getByText('Lucagiros arrow_drop_down').click();
-    await expect(page.getByRole('option', { name: 'Todos' })).toBeVisible();
 
-    await page.getByText('Selecciona 1 o más estados').click();
-    await expect(page.getByRole('option', { name: 'Pendiente' })).toBeVisible();
+    await page.getByText('Selecciona 1 o más estados').click();  
+    await page.getByRole('option', { name: 'Pagada' }).click();
+    await page.locator('#selector_remittance_state').click();
 
-    await page.getByText('Rechazada arrow_drop_down').click();
     await page.getByText('Selecciona rango o fecha').click();
-    await expect(page.getByRole('heading', { name: 'Fecha de operación' })).toBeVisible();
-
     await page.locator('#month_1_day_7').getByText('7', { exact: true }).click();
     await page.locator('#month_1_day_27').click();
     await page.getByRole('button', { name: 'Aceptar' }).click();
     await page.getByRole('button', { name: 'Buscar' }).click();
-    await expect(page.getByRole('heading', { name: 'Resultados de búsqueda' })).toBeVisible();
+
     await page.getByRole('link', { name: 'Cerrar sesión' }).click();
 
 });
